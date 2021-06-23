c19c6f9ff89f593c86f28907d89587dcb7ca52da
TatianaFilimonova <filimonovatatiana17@gmail.com>
2021-06-23 12:06:06 +0300

    ver 3

--- Laba5.Farmer/laba5.ver2.py
+++ Laba5.Farmer/laba5.ver2.py
@@ -5,7 +5,7 @@ from scipy.optimize import linprog
 var = int(input("Enter the number of your option: "))
 n = var + 1
 
-Yield = []
+Yielddd = []
 available = []
 profit = []
 prod_name = []
@@ -13,7 +13,7 @@ rb = xlrd.open_workbook(r'FARMER.xls', formatting_info=True)
 sheet = rb.sheet_by_index(0)
 k = 1
 for i in range(k, k+6):
-    Yield.append(sheet.row_values(n)[i])
+    Yielddd.append(sheet.row_values(n)[i])
 k += 6
 for i in range(k, k+6):
     available.append(sheet.row_values(n)[i])
@@ -25,7 +25,7 @@ for i in range(1, 7):
 
 cost = [0 for k in range(6)]  # стоит 1 га урожая
 for i in range(6):
-    cost[i] = -1 * Yield[i] * profit[i]
+    cost[i] = -1 * Yielddd[i] * profit[i]
 
 tot1_eq = numpy.ones([1, len(cost)])
 tot2_eq = [10]
